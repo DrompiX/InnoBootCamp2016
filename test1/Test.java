8bea0fea1dc85e9ba4995f0ea6e0d213fae061df
DrompiX <mrlocky98@gmail.com>
2016-07-31 21:48:09 +0300

    New

--- test1/Test.java
+++ test1/Test.java
@@ -1,5 +1,6 @@
 class Test {
     public static void main(String args[]) {
-        System.out.println("Hello, InnoBootCamp2016!");
+        System.out.println("Hello, InnoBootCamp2016!\n");
+        System.out.println("I want to sleep... zzz...");
     }
 }
