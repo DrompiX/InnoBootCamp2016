7d2fb9eacec506b8ef819f6d9adae51595b2ff47
DrompiX <mrlocky98@gmail.com>
2016-07-31 23:19:29 +0300

    Last

--- test1/Test.java
+++ test1/Test.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 class Test {
-    public static void main(String args[]) {
-        System.out.println("Hello, InnoBootCamp2016!\n");
-        System.out.println("I want to sleep... zzz...");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");    
+        }
     }
 }
