0a4c95354df6ab9c86af6a707e98b2dc4a160416
DrompiX <mrlocky98@gmail.com>
2016-07-31 23:28:43 +0300

    New

---
+++ day1/New.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class New {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println("Our number is: " + n);
+    }
+}
