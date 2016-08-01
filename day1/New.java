f20fe4b9f30cfb59ae1ee9255769ff81d1550568
DrompiX <mrlocky98@gmail.com>
2016-08-01 14:58:39 +0300

    New tasks

--- day1/New.java
+++ day1/New.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 class New {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        System.out.println("Our number is: " + n);
+        Scanner inp = new Scanner(System.in); 
+        
     }
 }
