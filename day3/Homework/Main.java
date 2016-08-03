938ca1b3061ef71a3c043b0f33e243660c557a38
DrompiX <mrlocky98@gmail.com>
2016-08-03 16:16:02 +0300

    Deleted

--- day3/Homework/Main.java
+++ day3/Homework/Main.java
@@ -3,15 +3,15 @@ import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        //Scanner in = new Scanner(new BufferedReader(new FileReader("input.txt")));
-        //PrintWriter out = new PrintWriter(new FileWriter("output.txt"));
+        Scanner in = new Scanner(new BufferedReader(new FileReader("input.txt")));
+        PrintWriter out = new PrintWriter(new FileWriter("output.txt"));
         TaskA solver = new TaskA();
-        solver.solve();//in, out);
+        solver.solve(in, out);
         out.close();
     }
 
     static class TaskA {
-        public void solve(){//Scanner in, PrintWriter out) {
+        public void solve() Scanner in, PrintWriter out) {
             int num = in.nextInt();
             for (int i = 1; i < num; i++) {
                 if (num % i == 0)
